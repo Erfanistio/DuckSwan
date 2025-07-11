4392150fc1b91a906606ac4a2d9afec5569c1ead
Erfan Akrami <noterfanistio@gmail.com>
2025-07-11 19:23:26 +0330

    Final

---
+++ script.js
@@ -0,0 +1,45 @@
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+mobileMenu.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+});
+(function () {
+  function c() {
+    var b = a.contentDocument || a.contentWindow.document;
+    if (b) {
+      var d = b.createElement("script");
+      d.innerHTML =
+        "window.__CF$cv$params={r:'95d87c5f07749b9b',t:'MTc1MjIzODg0OC4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";
+      b.getElementsByTagName("head")[0].appendChild(d);
+    }
+  }
+  if (document.body) {
+    var a = document.createElement("iframe");
+    a.height = 1;
+    a.width = 1;
+    a.style.position = "absolute";
+    a.style.top = 0;
+    a.style.left = 0;
+    a.style.border = "none";
+    a.style.visibility = "hidden";
+    document.body.appendChild(a);
+    if ("loading" !== document.readyState) c();
+    else if (window.addEventListener)
+      document.addEventListener("DOMContentLoaded", c);
+    else {
+      var e = document.onreadystatechange || function () {};
+      document.onreadystatechange = function (b) {
+        e(b);
+        "loading" !== document.readyState &&
+          ((document.onreadystatechange = e), c());
+      };
+    }
+  }
+})();
